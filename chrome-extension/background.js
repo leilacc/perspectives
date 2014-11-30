89af4f1e99f8699a013e6f58580ddad119ee7a6a
Jelle Akkerman <jelle@jelleakkerman.nl>
2014-11-30 13:29:32 +0300

    adding animation

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,6 +1,7 @@
 chrome.browserAction.onClicked.addListener(function(){
+    chrome.tabs.executeScript(null, {file: "bower_components/velocity/velocity.js"});
+    chrome.tabs.executeScript(null, {file: "bower_components/react/react.min.js"});
+
     chrome.tabs.insertCSS(null, {file: "style.css"});
-    chrome.tabs.executeScript(null, {file: "bower_components/react/react.js"});
-    chrome.tabs.executeScript(null, {file: "bower_components/react/JSXTransformer.js"});
     chrome.tabs.executeScript(null, {file: "build/app.js"});
 })
