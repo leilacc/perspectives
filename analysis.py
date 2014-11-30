a5cce4603e50017cad6e2643623213ca819e10a0
Leila Chan Currie <me@leila.cc>
2014-11-30 11:29:05 +0300

    basic loaded word synonyms

--- analysis.py
+++ analysis.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import json
 import nltk
 from nltk.corpus import wordnet as wn
 from nltk.chunk.regexp import *
@@ -35,7 +36,6 @@ def form_wn_phrase(NPleaves):
   phrase = []
   for leaf in NPleaves:
     phrase.append(leaf[0])
-  print phrase
   return '_'.join(phrase)
 
 def check_wn_phrase(NPleaves):
@@ -51,48 +51,72 @@ def check_wn_phrase(NPleaves):
     # remove leading numbers,adverbs, and adjectives that do not form a
     # commonly-used phrase according to WN
     NPleaves = NPleaves[1:]
-  print NPleaves
   return NPleaves
 
 def get_NPs(sentence):
   tokens = nltk.word_tokenize(sentence)
   tagged_tokens = nltk.pos_tag(tokens)
   NPs = chunk(tagged_tokens)
+  return NPs
 
 def chunk(pos_tagged_tokens):
   chunks = []
-  cur_chunk = []
   tree = NpChunker.parse(pos_tagged_tokens)
   for child in tree:
     if type(child) == nltk.Tree and child.node == 'NP':
       leaves = child.leaves()
-      print leaves
-      cur_chunk.append(leaves_to_str(check_wn_phrase(leaves)))
-  if cur_chunk:
-    # catch the last chunk
-    chunks.append(''.join(cur_chunk))
+      chunks.append(leaves_to_str(check_wn_phrase(leaves)))
 
   return chunks
 
-def syn(word1, word2):
+def get_synsets(np):
+  tokens = nltk.word_tokenize(np)
+  synsets = wn.synsets('_'.join(tokens))
+  while np and not synsets:
+    # remove modifiers from noun head
+    tagged_np = nltk.pos_tag(tokens)
+    if not tagged_np[0][1].startswith('NN'):
+      tokens = tokens[1:]
+    else:
+      break
+    synsets = wn.synsets('_'.join(tokens))
+
+  return synsets
+
+def syn(np1, np2):
   max_lch = 0
   max_syns = ()
-  for syn1 in wn.synsets(word1):
-    for syn2 in wn.synsets(word2):
-      try:
-        lch = syn1.lch_similarity(syn2)
-        if lch > max_lch:
-          max_syns = (syn1, syn2)
-          max_lch = lch
-      except:
-        continue
-  if max_lch > 1.5:
+  syn1 = get_synsets(np1)[0]
+  syn2 = get_synsets(np2)[0]
+  try:
+    if syn1 != syn2:
+      lch = syn1.lch_similarity(syn2)
+      if lch > max_lch:
+        max_syns = (syn1, syn2)
+        max_lch = lch
+  except:
+    pass
+  if max_lch > 1.6:
     return max_syns
 
-def compare_NPs(article1, article2):
-  set(get_NPs(article.headline) + get_NPs(article.text))
+def get_NP_syns(article1_NPs, article2_NPs):
+  syns = []
+  for np1 in article1_NPs:
+    for np2 in article2_NPs:
+      if np1 in LOADED_WORDS and np2 in LOADED_WORDS:
+        syn_match = syn(np1, np2)
+        if syn_match:
+          syns.append(syn_match)
 
+  return syns
 
 
 if __name__ == '__main__':
-  print get_NPs('ICC finds Israel guilty of war crimes in Mavi Marmara raid')
+  with open('article1.json') as article1:
+    article1 = json.load(article1)
+    article1_NPs = set(get_NPs(article1["title"]) + get_NPs(article1["article_text"]))
+
+    with open('article2.json') as article2:
+      article2 = json.load(article2)
+      article2_NPs = set(get_NPs(article2["title"]) + get_NPs(article2["article_text"]))
+      print get_NP_syns(article1_NPs, article2_NPs)
