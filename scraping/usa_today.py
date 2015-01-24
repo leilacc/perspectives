cf76d3507e68c498c6d20745da4eed34792ac7a5
Leila Chan Currie <me@leila.cc>
2015-01-24 16:09:48 -0500

    Debugged, and added logging

--- scraping/usa_today.py
+++ scraping/usa_today.py
@@ -1,10 +1,13 @@
+import logging
 import requests
 import xmltodict
 
+from logger import log
 import news_interface
 import news_orgs
 import api_keys
 
+logging.basicConfig(filename='usa_today.log', level=logging.WARNING)
 
 class USAToday(news_interface.NewsOrg):
   '''Methods for interacting with the USA Today website/API.'''
@@ -14,17 +17,22 @@ class USAToday(news_interface.NewsOrg):
     return 'got it'
 
   def get_query_results(self, query):
-    '''Implementation for getting an article from USA Today.'''
-    res = requests.post(
+    '''Implementation for keyword searches from USA Today.'''
+    res = requests.get(
         "http://api.usatoday.com/open/articles?keyword=%s&api_key=%s"
         % (query, api_keys.api_keys[news_orgs.USA_TODAY]))
-    print res.text
     xml_dict = xmltodict.parse(res.text)
-    all_articles = xml_dict['rss']['channel']['item']
+
+    try:
+      all_articles = xml_dict['rss']['channel']['item']
+    except KeyError:
+      log.error(res.text)
+
     top_articles = []
-    for article in all_articles[0:NUM_ARTICLES]:
+    for article in all_articles[0:news_interface.NUM_ARTICLES]:
       headline = article['title']
       link = article['link']
       body = self.get_article(link)
-      top_articles.append(Article(headline, body, link, USA_TODAY))
+      top_articles.append(
+          news_interface.Article(headline, body, link, news_orgs.USA_TODAY))
     return top_articles
