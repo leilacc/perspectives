dc1326b7da1f7416e7e77bc21e8eed5252ec56ae
Leila Chan Currie <me@leila.cc>
2015-01-24 16:53:41 -0500

    get_article complete

--- scraping/usa_today.py
+++ scraping/usa_today.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 import logging
 import requests
 import xmltodict
@@ -13,11 +14,26 @@ class USAToday(news_interface.NewsOrg):
   '''Methods for interacting with the USA Today website/API.'''
 
   def get_article(self, url):
-    '''Implementation for getting an article from USA Today.'''
-    return 'got it'
+    '''Implementation for getting an article from USA Today.
+
+    url: A URL in the http://www.usatoday.com/story/* domain.
+
+    Returns: The Article representing the article at that url.
+    '''
+    soup = BeautifulSoup(requests.get(url).text)
+    article = soup.article
+    headline = article.h1.string
+    paragraphs = article.find_all('p', attrs={'class': None})
+    body = ' '.join([p.get_text() for p in paragraphs])
+    return news_interface.Article(headline, body, url, news_orgs.USA_TODAY)
 
   def get_query_results(self, query):
-    '''Implementation for keyword searches from USA Today.'''
+    '''Implementation for keyword searches from USA Today.
+
+    query: A URL-encoded string.
+
+    Returns: A list of the top Articles returned by the query search.
+    '''
     res = requests.get(
         "http://api.usatoday.com/open/articles?keyword=%s&api_key=%s"
         % (query, api_keys.api_keys[news_orgs.USA_TODAY]))
