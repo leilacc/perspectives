f6cfd9a4dda37a07488db66569995678aedc4a7f
Leila Chan Currie <me@leila.cc>
2015-01-24 16:10:04 -0500

    Set logging preferences

---
+++ scraping/logger.py
@@ -0,0 +1,10 @@
+import logging
+import sys
+
+# Log INFO and higher to stdout
+log = logging.getLogger(__name__)
+out_hdlr = logging.StreamHandler(sys.stdout)
+out_hdlr.setFormatter(logging.Formatter('%(asctime)s %(message)s'))
+out_hdlr.setLevel(logging.INFO)
+log.addHandler(out_hdlr)
+log.setLevel(logging.INFO)
