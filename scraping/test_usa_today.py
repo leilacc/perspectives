6aeb91e80ebc09d4bbf31a820f09e382a9cecac4
Leila Chan Currie <me@leila.cc>
2015-01-24 16:53:55 -0500

    Tests for get_article

--- scraping/test_usa_today.py
+++ scraping/test_usa_today.py
@@ -8,6 +8,18 @@ class TestUSAToday(unittest.TestCase):
   def setUp(self):
     self.USAToday = usa_today.USAToday()
 
+  def test_get_article_news(self):
+    url = 'http://www.usatoday.com/story/news/nation-now/2015/01/20/democrat-charlie-hebdo-tribute-obama-speech/22070673/'
+    article = self.USAToday.get_article(url)
+    self.assertTrue(isinstance(article, news_interface.Article),
+        'Expected the result to be an Article instance')
+
+  def test_get_article_travel(self):
+    url = 'http://www.usatoday.com/story/travel/columnist/greatamericanbites/2015/01/22/dierks-bently-whiskey-row-scottsdale/22112457/'
+    article = self.USAToday.get_article(url)
+    self.assertTrue(isinstance(article, news_interface.Article),
+        'Expected the result to be an Article instance')
+
   def test_get_query_results(self):
     query = 'charlie+hebdo'
     res = self.USAToday.get_query_results(query)
