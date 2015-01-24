f22e0900b78e9495ae33a9ed556d8435e9ca6710
Leila Chan Currie <me@leila.cc>
2015-01-24 16:10:24 -0500

    Test for get_query_results passes

--- scraping/test_usa_today.py
+++ scraping/test_usa_today.py
@@ -1,4 +1,6 @@
 import unittest
+
+import news_interface
 import usa_today
 
 class TestUSAToday(unittest.TestCase):
@@ -6,13 +8,13 @@ class TestUSAToday(unittest.TestCase):
   def setUp(self):
     self.USAToday = usa_today.USAToday()
 
-  def test_get_query_result(self):
+  def test_get_query_results(self):
     query = 'charlie+hebdo'
-    self.USAToday.get_query_result(query)
-    #self.assertEqual( multiply(3,4), 12)
-
-  #def test_strings_a_3(self):
-    #self.assertEqual( multiply('a',3), 'aaa')
+    res = self.USAToday.get_query_results(query)
+    self.assertEqual(len(res), news_interface.NUM_ARTICLES,
+        'Expected %d articles' % news_interface.NUM_ARTICLES)
+    self.assertTrue(isinstance(res[0], news_interface.Article),
+        'Expected the result to be an Article instance')
 
 if __name__ == '__main__':
   unittest.main()
