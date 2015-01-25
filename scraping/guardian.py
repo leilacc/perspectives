037d9c0512dda2cd1ee2db1ef945adce54f8b053
Leila Chan Currie <me@leila.cc>
2015-01-25 16:49:17 -0500

    Scraper and tests for Guardian

--- scraping/guardian.py
+++ scraping/guardian.py
@@ -12,7 +12,7 @@ logging.basicConfig(filename='guardian.log', level=logging.WARNING)
 
 
 class Guardian(news_interface.NewsOrg):
-  '''Methods for interacting with the Guardian website.'''
+  '''Methods for interacting with the Guardian website/API.'''
 
   def get_article(self, url):
     '''Implementation for getting an article from the Guardian.
