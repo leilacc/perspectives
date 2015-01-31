bd263cf014279e2d3e8bdda85fe90f43c9d8a739
Leila Chan Currie <me@leila.cc>
2015-01-31 18:55:20 -0500

    Refactored get_query into extract_keywords. Same functionality, but encapsulated in a function.

--- analysis/extract_keywords.py
+++ analysis/extract_keywords.py
@@ -1,62 +1,47 @@
+'''Extracts key words from a news article headline.'''
+
 import nltk
-import json
 
-with open('output_json_files/jpost_output.json') as data_file: 
-    data = json.load(data_file)
-
-query = data["title"]
-
-
-
-text_pre = query #"""What really happened with NBC and Ayman Mohyeldin"""
-text_pre = text_pre.lower()
-from nltk.corpus import stopwords
-stopwords = stopwords.words('english')
-text = ""
-for word in text_pre.split(" "):
-    if word not in stopwords:
-        text += word + " "
-
-#print text
-
-#The Buddha, the Godhead, resides quite as comfortably in the circuits of a digital
-#computer or the gears of a cycle transmission as he does at the top of a mountain
-#or in the petals of a flower. To think otherwise is to demean the Buddha...which is
-#to demean oneself."""
- 
+STOPWORDS = nltk.corpus.stopwords.words('english')
 # Used when tokenizing words
-sentence_re = r'''(?x)      # set flag to allow verbose regexps
-      ([A-Z])(\.[A-Z])+\.?  # abbreviations, e.g. U.S.A.
+SENTENCE_RE = r'''
+    (?x)                    # set flag to allow verbose regexps
+    ([A-Z])(\.[A-Z])+\.?    # abbreviations, e.g. U.S.A.
     | \w+(-\w+)*            # words with optional internal hyphens
     | \$?\d+(\.\d+)?%?      # currency and percentages, e.g. $12.40, 82%
     | \.\.\.                # ellipsis
     | [][.,;"'?():-_`]      # these are separate tokens
 '''
- 
-lemmatizer = nltk.WordNetLemmatizer()
-stemmer = nltk.stem.porter.PorterStemmer()
- 
-#Taken from Su Nam Kim Paper...
-grammar = r"""
-    NBAR:
-        {<NN.*|JJ>*<NN.*>}  # Nouns and Adjectives, terminated with Nouns
-        
-    NP:
-        {<NBAR>}
-        {<NBAR><IN><NBAR>}  # Above, connected with in/of/etc...
-"""
-chunker = nltk.RegexpParser(grammar)
-
-        
-toks = nltk.regexp_tokenize(text, sentence_re)
-postoks = nltk.tag.pos_tag(toks)
 
 
-query = ""
+def remove_stopwords(sentence):
+  '''Removes the stopwords from a sentence.
 
-for (word,tag) in postoks:
+  sentence: A string.
+
+  Returns: sentence, unchanged except for having stopwords removed.
+  '''
+  content_words = []
+  for word in sentence.split():
+    if word not in STOPWORDS:
+      content_words.append(word)
+  return ' '.join(content_words)
+
+def extract_keywords(headline):
+  '''Extracts key words from a news article headline.
+
+  headline: A string.
+
+  Returns: A string of keywords extracted from headline.
+  '''
+  headline = headline.lower()
+  content_headline = remove_stopwords(headline)
+  tokens = nltk.regexp_tokenize(content_headline, SENTENCE_RE)
+  pos_tokens = nltk.tag.pos_tag(tokens)
+
+  keywords = []
+  for (word, tag) in pos_tokens:
     if tag == "NN":
-        query += word + " "
+      keywords.append(word)
 
-
-print query
+  return ' '.join(keywords)
