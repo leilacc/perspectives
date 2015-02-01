16fd18ae2d10ef9fb3c156bba1332ddb4399e98e
Leila Chan Currie <me@leila.cc>
2015-02-01 01:48:55 -0500

    Cleaned imports, return, docstrings. Added decoding

--- analysis/scraping/times_of_israel.py
+++ analysis/scraping/times_of_israel.py
@@ -1,58 +1,48 @@
 from bs4 import BeautifulSoup
-import json
 import logging
 import requests
+import urllib2
 
-
+import helpers
 from logger import log
 import news_interface
 import news_orgs
 
-
-from BeautifulSoup import BeautifulSoup
-import urllib2
-import codecs
-import copy
-import re
-import argparse
-import sys
-import json
-import wget
-from selenium import webdriver
-
-
 logging.basicConfig(filename='times_of_israel.log', level=logging.WARNING)
 
-class TIMES_OF_ISRAEL(news_interface.NewsOrg):
+class TimesOfIsrael(news_interface.NewsOrg):
   '''Methods for interacting with the Times of Israel website.'''
 
   def get_article(self, url):
-    '''Implementation for getting an article from Times of Israel.                                                                                                                                                                                                                                                                      
-                                                                                                                                                                                                                                                                                                                           
-    url: A URL in the www.timesofisrael.* domain.                                                                                                                                                                                                                                                                                     
-                                                                                                                                                                                                                                                                                                                            
-    Returns: The Article representing the article at that url.                                                                                                                                                                                                                                                              
+    '''Implementation for getting an article from Times of Israel.
+
+    Args:
+      url: A URL in the www.timesofisrael.com/* domain.
+
+    Returns:
+      The Article representing the article at that url.
     '''
     response = urllib2.urlopen(url)
     html = response.read()
     soup = BeautifulSoup(html)
 
-
     a = soup.find("title")
-    headline = a.text
+    headline = helpers.decode(a.text)
     paragraphs = soup.findAll("p", {"itemprop": "articleBody"})
-    body = ' '.join([p.text.encode('ascii', 'ignore') for p in paragraphs])
+    body = ' '.join([helpers.decode(p.text) for p in paragraphs])
 
     log.info(headline)
     log.info(body)
     return news_interface.Article(headline, body, url, news_orgs.TIMES_OF_ISRAEL)
 
   def get_query_results(self, query):
-    '''Implementation for keyword searches from Times of Israel                                                                                                                                                                                                                                                                        
-                                                                                                                                                                                                                                                                                                                            
-    query: A URL-encoded string.                                                                                                                                                                                                                                                                                            
-                                                                                                                                                                                                                                                                                                                            
-    Returns: A list of the top Articles returned by the query search.                                                                                                                                                                                                                                                       
+    '''Implementation for keyword searches from Times of Israel.
+
+    Args:
+      query: A URL-encoded string.
+
+    Returns:
+      A list of the top Articles returned by the query search.
     '''
     res = requests.get("https://www.googleapis.com/customsearch/v1element?key=AIzaSyCVAXiUzRYsML1Pv6RwSG1gunmMikTzQqY&rsz=filtered_cse&num=10&hl=en&prettyPrint=false&source=gcsc&gss=.com&sig=23952f7483f1bca4119a89c020d13def&cx=015742192883069867459:k3m1yn-i4ua&q=%s&googlehost=www.google.com&callback=google.search.Search.apiary782&nocache=1422549552720" % (query))
 
@@ -66,7 +56,7 @@ class TIMES_OF_ISRAEL(news_interface.NewsOrg):
       except:
         pass
 
-    print article_urls
     top_articles = []
     for url in article_urls[0:news_interface.NUM_ARTICLES]:
       top_articles.append(self.get_article(url))
+    return top_articles
