a19e93d8a0e4011a3253aae0f4b0491564ec0fca
Leila Chan Currie <me@leila.cc>
2015-02-01 01:08:35 -0500

    cleaned up

--- analysis/scraping/jpost.py
+++ analysis/scraping/jpost.py
@@ -1,61 +1,49 @@
-#from bs4 import BeautifulSoup
-#import simplejson as json
-import json
+from bs4 import BeautifulSoup
 import logging
 import requests
-
+import urllib2
 
 from logger import log
 import news_interface
 import news_orgs
 
-
-from BeautifulSoup import BeautifulSoup
-import urllib2
-import codecs
-import copy
-import re
-import argparse
-import sys
-
-
-
-
 logging.basicConfig(filename='jpost.log', level=logging.WARNING)
 
-class JPOST(news_interface.NewsOrg):
+class JPost(news_interface.NewsOrg):
   '''Methods for interacting with the JPOST website.'''
 
   def get_article(self, url):
     '''Implementation for getting an article from JPost.
-                                                                                                                                                                                                                                                                                                                            
-    url: A URL in the www.jpost.* domain.                                                                                                                                                                                                                                                                                     
-                                                                                                                                                                                                                                                                                                                            
-    Returns: The Article representing the article at that url.                                                                                                                                                                                                                                                              
+
+    Args:
+      url: A URL in the www.jpost.com/* domain.
+
+    Returns:
+      The Article representing the article at that url.
     '''
 
     response = urllib2.urlopen(url)
     html = response.read()
     soup = BeautifulSoup(html)
-    a = soup.find("title")
-    headline = a.text
+    a = soup.find('h1', attrs={'class': 'article-title'})
+    headline = a.text.encode('ascii', 'ignore').strip().strip('\r\n')
     paragraphs = soup.find("div", {"class": "article-text"})
     article = paragraphs.find("p")
 
     body = article.text.encode('ascii', 'ignore')
-
 
     log.info(headline)
     log.info(body)
     return news_interface.Article(headline, body, url, news_orgs.JPOST)
 
   def get_query_results(self, query):
-    '''Implementation for keyword searches from JPOST.                                                                                                                       
-                                                                                                                                                 
-    query: A URL-encoded string. 
-    
-    Returns: A list of the top Articles returned by the query search.                                                                                                           
-                                                                                                                                            
+    '''Implementation for keyword searches from JPost.
+
+    Args:
+      query: A URL-encoded string.
+
+    Returns:
+      A list of the top Articles returned by the query search.
     '''
     res = requests.get("https://www.googleapis.com/customsearch/v1?key=AIzaSyCMGfdDaSfjqv5zYoS0mTJnOT3e9MURWkU&cx=012860551684240964068:7b9pexdovug&q=%s&start=1&callback=getCSEResults" % (query))
     output = res.text.encode('ascii', 'ignore').split("\n")
