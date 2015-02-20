2430c7023072c3beca2bfbbad8ae4ac90d40b1b6
Jelle Akkerman <m@jelle.io>
2015-02-21 00:11:03 +0100

    first idea, meh

--- chrome-extension/app.js
+++
@@ -1,8 +0,0 @@
-var hookElem = document.createElement("div");
-hookElem.setAttribute("id", "react-hook");
-document.body.appendChild(hookElem);
-
-React.render(
-        <h1>Hello, world!</h1>,
-        document.getElementById('react-hook')
-      );
