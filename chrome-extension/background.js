104d64425edde26a9f4439813c97e0631928a247
Jelle Akkerman <m@jelle.io>
2015-04-27 09:27:23 +0200

    new ext with api in place

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,7 +1,125 @@
-chrome.browserAction.onClicked.addListener(function(){
-    chrome.tabs.executeScript(null, {file: "bower_components/velocity/velocity.js"});
-    chrome.tabs.executeScript(null, {file: "bower_components/react/react.min.js"});
+// if news website loaded then...
+//var counter = 0
 
-    chrome.tabs.insertCSS(null, {file: "style.css"});
+//
+//bal = chrome.browserAction.setBadgeText
+//setInterval(function(){
+//    counter= counter+1
+//    bal({text: ""+counter})},1000)
+
+var data
+
+var startRequest = function(tab){
+    console.log("start server request")
+    console.time("server request");
+    var request = new XMLHttpRequest();
+    request.open('GET', 'http://52.1.96.53/analysis/r.php?start_point='+ tab.url, true);
+
+    request.onload = function() {
+        console.timeEnd("server request");
+        if (request.status >= 200 && request.status < 400) {
+            // Success!
+            console.log(request)
+            var data = request.responseText.match(/\[([^]+)/)
+
+            if (data.length > 0) {
+                var parsed = JSON.parse(data[0])
+
+                turning = false
+
+                datawonull = parsed.filter(function (f) {
+                    return (f !== null)
+                })
+
+                chrome.tabs.insertCSS(null, {file: "style.css"});
+
+                chrome.browserAction.setBadgeText({text: "" + datawonull.length})
+
+                chrome.tabs.sendMessage(tab.id, datawonull, function (response) {
+                    console.log(response.farewell)
+                });
+            } else{
+                turning = false
+            }
+        } else {
+            // We reached our target server, but it returned an error
+            turning = false
+        }
+    };
+
+    request.onerror = function() {
+        console.timeEnd("server request");
+    };
+
+    request.send();
+}
+
+var turning = false;
+
+chrome.browserAction.setBadgeBackgroundColor({color:[0, 0, 0, 2]});
+
+chrome.browserAction.onClicked.addListener(function(){
+    turning = !turning;
+
+
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        startRequest(tabs[0])
+    });
+    chrome.tabs.executeScript(null, {file: "bower_components/velocity/velocity.js"});
+    chrome.tabs.executeScript(null, {file: "bower_components/react/react.js"});
     chrome.tabs.executeScript(null, {file: "build/app.js"});
-})
+
+});
+
+var r = 0
+delta = 1;
+pImageLoaded = false;
+var pImage = new Image(38, 38);
+pImage.src = 'p-38x38.png';
+pImage.loaded = function(){pImageLoaded = true};
+
+function draw(scale) {
+    if (turning === true) {
+        r++
+        //r += delta;
+        //if (r <= 0 || r >= 30) delta = -delta;
+    }
+
+    var canvas = document.getElementById('canvas')
+    var c2     = canvas.getContext('2d'),
+        numberOfSides = 8,
+        size   = 8.6*scale,
+        Xcenter = 9*scale,
+        Ycenter = 9*scale;
+
+    c2.clearRect(0,0,38,38);
+
+    if (!turning) c2.fillStyle = '#333';
+    else c2.fillStyle = '#00bbff'
+
+    c2.save();
+    c2.translate(Xcenter, Ycenter);
+    c2.rotate(r * (Math.PI/45));
+    c2.beginPath();
+
+
+    c2.moveTo (size * Math.cos(0), size *  Math.sin(0));
+
+    for (var i = 1; i <= numberOfSides;i += 1) {
+        c2.lineTo (size * Math.cos(i * 2 * Math.PI / numberOfSides), size * Math.sin(i * 2 * Math.PI / numberOfSides));
+    }
+
+
+    c2.closePath();
+    c2.fill();
+    c2.restore();
+    if (scale === 2) c2.drawImage(pImage, 0, 0);
+    else c2.drawImage(pImage, 0, 0, 19, 19);
+
+    return c2.getImageData(-3, -2, 19*scale, 19*scale);
+}
+
+window.setInterval(function() {
+    chrome.browserAction.setIcon({imageData: {'19': draw(1), '38': draw(2)
+    }});
+}, 50, chrome);
--- chrome-extension/src/app.js
+++ chrome-extension/src/app.js
@@ -1,39 +1,74 @@
-if (!hookElem) {
-    document.body.setAttribute("id","currentpage")
-    document.head.insertAdjacentHTML('afterend',"<div id='react-hook'></div>");
-    var hookElem = document.body.querySelectorAll("#react-hook")
-}
+var currentbody = document.body
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (!hookElem) {
+            document.body.setAttribute("id","currentpage")
+            document.body.setAttribute("style","transition: transform 0.2s ease-in-out;transform:translateX(-150px)")
+            document.head.insertAdjacentHTML('afterend',"<div id='perspectives'></div>")
+            var hookElem = document.querySelectorAll("#perspectives")[0]
+        }
+
+        React.render(<Main data={request}/>, hookElem);
+});
+
+//<div className="visible-on-hover">
+//    <h2>Horrific Images Capture The Sheer Brutality Of War In Ukraine</h2>
+//    <p className="details">Buzzfeed <span className="pull-right">3 days ago</span></p>
+//</div>
 
 var Article = React.createClass({
     componentDidMount: function(){
-        Velocity(this.getDOMNode(), {translateX: "0px"}, {duration:200, delay: 200+(50*this.props.num), easing:"ease-in-sine"})
+        Velocity(this.getDOMNode(), {translateX: "0px"}, {duration:200, delay: 200+(50*(this.props.num)), easing:"ease-in-sine"})
     },
     render: function(){
-        return (<li style={{backgroundColor: "rgba(255,255,255,0."+ (1+this.props.num)*2 +")"}}><h2>Title</h2></li>)
+        var re = /\*{2}(\w+)\*{2}/g;
+        var body = this.props.art.sentences[0].replace(re, "<b>$1</b>")
+
+        return (<li>
+            <div className="hidden-on-hover" dangerouslySetInnerHTML={{__html: "<p>" + body + "</p>"}}>
+            </div>
+        </li>)
     }
 })
 
+var arts = [
+            "“On Tuesday, however, <b>rebels</b> seized most of the town and took several Ukrainian soldiers captive.”",
+            "“The rebels had just <b>seized the town</b>, cutting Debaltseve off from the last road leading to Ukrainian territory.”",
+            "“When Russian-backed rebels went on the <b>offensive</b> in east Ukraine a month ago, the focal point of the <b>clashes</b> quickly switched to Debaltseve, a strategically key rail junction linking their two unsanctioned states.”",
+            "“For weeks, Ukraine’s <b>government denied</b> rebel claims to have the town surrounded, even as artillery fire prompted most civilians to flee, killed hundreds, and destroyed the town beyond recognition.”",
+            "“The soldiers were mostly local volunteers, though their <b>commanding officers were Russian</b> — as were the men who delivered them tanks and artillery.”"
+];
+
 var Main = React.createClass({
     getInitialState: function(){
-       return {offsetY: document.body.scrollTop}
+        return {firstTimeShowToggle: true,
+                articles: arts}
     },
     componentDidMount: function(){
-        var self = this
-        document.body.onscroll = function(){
-            console.log(document.body.scrollTop)
-            self.setState({offsetY: document.body.scrollTop})
+        //setTimeout(function(s){s.setState({articles: arts})}, 2000, this);
+    },
+    onHover: function(){
+        currentbody.setAttribute("style","transition: transform 0.2s ease-in-out;transform:translateX(-150px)")
+    },
+    onHoverOut: function(){
+        if(this.state.firstTimeShowToggle) {
+            this.setState({firstTimeShowToggle: false})
         }
+        currentbody.setAttribute("style","transition: transform 0.2s ease-in-out;transform:translateX(-25px)")
     },
     render: function(){
-        var articles = [1, 2, 3, 4, 5, 6, 7, 8].map(function(i){
-            return (<Article num={i}/>)
+        var articles = this.props.data.map(function(f,i){
+            return (<Article num={i} art={f}/>)
         })
 
-        return (<div id="perspectives-side" style={{transform: "translate(30vw,"+this.state.offsetY+"px)"}}>
-                    <ul>{articles}</ul>
+        var showToggle = this.state.firstTimeShowToggle ? "show" : "";
+
+        return (<div>
+                    <ul id="perspectives-side" onMouseOver={this.onHover} onMouseOut={this.onHoverOut} className={showToggle}>
+                        {articles}
+                    </ul>
                 </div>)
     }
 })
 
-React.render(<Main />,
-            document.getElementById('react-hook'));
